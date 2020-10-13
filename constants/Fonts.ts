730e165db1908477f04ef74d6289d62b991f8dd7
André Rebonato <andrerebonato913@hotmail.com>
2020-10-13 19:36:53 -0300

    Finished simple fonts constant

---
+++ constants/Fonts.ts
@@ -0,0 +1,17 @@
+interface IFonts {
+    primary: {
+        normal: string,
+        bold: string,
+        regular: string
+    }
+}
+
+const Fonts: IFonts = {
+    primary: {
+        normal: "Apercu",
+        bold: "ApercuBold",
+        regular: "ApercuRegular"
+    }
+}
+
+export default Fonts;
